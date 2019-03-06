70bb114d3bf4afbc619e0f2cc72c9cb281b91a2b
krison <krison_zhang@163.com>
2019-03-06 16:36:53 +0800

    小蜜蜂任务 生成注册和登陆记录-生成到生产库

--- bee_task/GenerateReg.py
+++ bee_task/GenerateReg.py
@@ -11,13 +11,14 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 class GenerateReg(object):
-    def __init__(self, host: str, port: int, user: str, password: str, db: str, charset: str):
-        if charset.strip() == '':
-            charset = 'utf8'
-
+    def __init__(self):
         # 初始化数据库
-        self.db = pymysql.connect(host=host, port=port, user=user, password=password, db=db, charset=charset)
-        self.login_db = pymysql.connect(host=host, port=port, user=user, password=password, db=db, charset=charset)
+        # 数仓数据库
+        self.db = pymysql.connect(host="47.101.147.20", port=3306, user="root", password="Big!!!data111",
+                                  db="dm_report", charset="utf8")
+        # 业务系统
+        self.business_db = pymysql.connect(host="172.19.200.63", port=3306, user="bigdata", password="6yhn7UJM",
+                                           db="loan_market", charset="utf8")
 
         # 初始化log
         log_fmt = '%(asctime)s\tFile \"%(filename)s\",line %(lineno)s\t%(levelname)s: %(message)s'
@@ -43,20 +44,28 @@ class GenerateReg(object):
         self.log.addHandler(log_console_handler)
 
     def get_user(self, num=0):
-        cursor = self.db.cursor()
+        b_cursor = self.business_db.cursor()
+        b_cursor.execute("select max(customer_id) from customer_info where register_channel = 'sys_reg'")
+        b_total = b_cursor.fetchone()[0]
+        if b_total:
+            b_total = str(b_total)
+        else:
+            b_total = "0"
 
+        cursor = self.db.cursor()
         get_user_sql = "select a.PHONE from dm_report.ind_info a " \
-                       "where PHONE not in(select mobile from Source_loan_market.customer_reg ) " \
+                       "where CUSTOMERID > '" + b_total + "'" \
                        "and PHONE is not null and PHONE <> '00000000000' and length(PHONE) > 10 limit " + str(num)
         cursor.execute(get_user_sql)
         rows = cursor.fetchall()
         users = []
         for row in rows:
             users.append(row[0])
+        self.log.info("要生成注册的用户手机号:{}".format(users))
         return users
 
     def reg_user(self, user_reg):
-        reg_sql = '''INSERT INTO `Source_loan_market`.`customer_reg` (
+        reg_sql = '''INSERT INTO `customer_info` (
                     `customer_id`,
                     `register_channel`,
                     `real_name`,
@@ -85,7 +94,7 @@ class GenerateReg(object):
                     )
                 '''
 
-        reg_cursor = self.db.cursor()
+        reg_cursor = self.business_db.cursor()
         for u in user_reg:
             user_id = 'MC' + time.strftime('%Y%m%d') + str(uuid.uuid1()).replace('-', '')[:19]
             # 5分钟之前的时间随机
@@ -94,7 +103,7 @@ class GenerateReg(object):
                 # 执行sql语句
                 reg_cursor.execute(reg_sql.format(user_id, u, before_time, before_time))
                 # 提交到数据库执行
-                self.db.commit()
+                self.business_db.commit()
             except Exception as e:
                 # Rollback in case there is any error
                 self.log.error(traceback.format_exc())
@@ -104,8 +113,8 @@ class GenerateReg(object):
         self.log.info("reg over")
 
     def user_login(self):
-        login_cursor = self.db.cursor()
-        login_cursor.execute("SELECT max(id) FROM customer_reg")
+        login_cursor = self.business_db.cursor()
+        login_cursor.execute("SELECT max(id) FROM customer_info")
         reg_count = int(login_cursor.fetchone()[0])
         # 9-22点13小时，每5分钟一次，每小时执行12次
         take_count = math.ceil(reg_count * random.randint(50, 60) / 100 / (13*12))
@@ -113,7 +122,7 @@ class GenerateReg(object):
 
         # 随机取出的id
         login_ids = [str(random.randint(1, reg_count)) for i in range(take_count)]
-        login_cursor.execute("SELECT id FROM customer_reg where id in({})".format(','.join(login_ids)))
+        login_cursor.execute("SELECT id FROM customer_info where id in({})".format(','.join(login_ids)))
         row_take = login_cursor.fetchall()
         login_take = []
         for row_i in row_take:
@@ -121,8 +130,9 @@ class GenerateReg(object):
         self.log.info(("要生成登陆的用户id:{}".format(login_take)))
 
         # 记录插入登陆表
-        login_sql = '''INSERT INTO `Source_loan_market`.`customer_login_history` (
+        login_sql = '''INSERT INTO `customer_login_history` (
                     `customer_id`,
+                    `login_terminal`,
                     `login_time`,
                     `create_time`,
                     `update_time`
@@ -130,6 +140,7 @@ class GenerateReg(object):
                 VALUES
                     (
                         '{}',
+                        'sys_login',
                         '{}',
                         now(),
                         now()
@@ -141,7 +152,7 @@ class GenerateReg(object):
                 # 执行sql语句
                 login_cursor.execute(login_sql.format(login_id, login_time))
                 # db注册操作提交
-                self.db.commit()
+                self.business_db.commit()
             except Exception as e:
                 # Rollback in case there is any error
                 self.log.error(traceback.format_exc())
@@ -153,17 +164,12 @@ class GenerateReg(object):
     def closes(self):
         # 关闭数据库连接
         self.db.close()
+        self.business_db.close()
 
 
 if __name__ == '__main__':
-    host = "47.101.147.20"
-    port = 3306
-    user = "root"
-    password = "Big!!!data111"
-    db = "Source_loan_market"
-    charset = 'utf8'
     # 初始化 db，log
-    gr = GenerateReg(host, port, user, password, db, charset)
+    gr = GenerateReg()
     # 随机选取3-7条记录
     user2reg = gr.get_user(5 + random.randint(-2, 2))
     # 选取用户生成注册
